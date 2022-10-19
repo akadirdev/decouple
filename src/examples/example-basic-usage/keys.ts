e6e4df8d139053e917c209cf573f758eaa5fbc0d
akadirdev <akadir.dd@gmail.com>
2022-10-19 13:59:14 +0300

    add binding key implementation

---
+++ src/examples/example-basic-usage/keys.ts
@@ -0,0 +1,5 @@
+import { BindingKey } from "../../types";
+
+export const USER_CONTROLLER = BindingKey.create("user.controller");
+export const USER_SERVICE = BindingKey.create("user.service");
+export const ROLE_SERVICE = BindingKey.create("role.service");
--- src/types.ts
+++ src/types.ts
@@ -5,9 +5,10 @@ export enum InjectAt {
   CONSTRUCTOR = "CONSTRUCTOR",
 }
 
-export interface Injectable<T> {
-  ctor: Constructor<T>;
-  property?: keyof T;
+export interface Injectable {
+  bindingKey: BindingKey;
+  ctor?: Constructor<any>;
+  property?: string;
   injectAt: InjectAt;
   parameterIndex?: number;
 }
@@ -21,12 +22,30 @@ export interface InjectableOptions {
   bindingScope: BindingScope;
 }
 
+export class BindingKey {
+  readonly _key: symbol;
+
+  private constructor(key: string) {
+    this._key = Symbol(key);
+  }
+
+  public static create(key: string): BindingKey {
+    return new BindingKey(key);
+  }
+}
+
 export class DependencyConstructor<T> {
   public ctor: Constructor<T>;
   private _bindingScope: BindingScope;
+  private _bindingKey: BindingKey;
 
-  constructor(ctor: Constructor<T>, bindingScope?: BindingScope) {
+  constructor(
+    ctor: Constructor<T>,
+    bindingKey: BindingKey,
+    bindingScope?: BindingScope
+  ) {
     this.ctor = ctor;
+    this._bindingKey = bindingKey;
     this._bindingScope = bindingScope ?? BindingScope.TRANSIENT;
   }
 
